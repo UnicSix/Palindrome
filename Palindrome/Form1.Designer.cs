089a2052904ca3fa541d7d5581e294c3d539a099
Noookia <matchman7980@gmail.com>
2023-10-26 23:07:12 +0800

    加入專案檔案。

---
+++ Palindrome/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Palindrome
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.dataGridView1.Location = new System.Drawing.Point(57, 72);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(422, 416);
+            this.dataGridView1.TabIndex = 8;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "是否回文";
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 80;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "字串";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 300;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(543, 534);
+            this.Controls.Add(this.dataGridView1);
+            this.Font = new System.Drawing.Font("新細明體", 9F);
+            this.Name = "Form1";
+            this.Text = "form";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+    }
+}
+
---
+++ Palindrome/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Palindrome
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            string inputFile = "text.txt";
+            InitializeComponent();
+            System.IO.StreamReader inputData = new System.IO.StreamReader(inputFile);
+            string line = "";
+            int row = 0;
+            line = inputData.ReadLine();
+            while (line != null)
+            {
+                string rev_str="";
+                for(int i=0; i<line.Length; i++)
+                {
+                    rev_str += line[line.Length - i - 1];
+                }
+                dataGridView1.Rows.Add("");
+                dataGridView1.Rows[row].Cells[0].Value = line;
+                if(rev_str == line)
+                    dataGridView1.Rows[row].Cells[1].Value = "是";
+                else
+                    dataGridView1.Rows[row].Cells[1].Value = "否";
+                row++;
+                line = inputData.ReadLine();
+            }
+            DataGridViewCellStyle cellStyle = new DataGridViewCellStyle();
+            cellStyle.Font = new System.Drawing.Font("Times New Roman", 15, System.Drawing.FontStyle.Regular); // Change the font size and style
+            dataGridView1.DefaultCellStyle = cellStyle;
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.ColumnHeadersVisible = false;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
